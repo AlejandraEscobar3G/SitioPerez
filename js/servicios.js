0a41379213d6e3dd53ec4172df64f4d470ce046e
AlejandraEscobar3G <ale.fesico@gmail.com>
2018-02-13 01:17:04 -0600

    Agrego eventos click para animacionse de seccion servicios

---
+++ js/servicios.js
@@ -0,0 +1,27 @@
+$(function() {
+    var toggle = 0;
+    $( "#transporte" ).click(
+        function() {
+            if(toggle == 0) {
+                var service = $("#services");
+                var stringHtml ='<div class="animation-square p-4" id="close">' +
+                                '<button type="button" class="close" aria-label="Close">'+
+                                    '<span aria-hidden="true">&times;</span>' +
+                                '</button>' +
+                            '</div>';
+                var htmlParse = $.parseHTML(stringHtml);
+                service.append(htmlParse);
+                toggle = 1;
+            }
+            $(".close").click(function() {
+                $("#close").remove();
+                toggle = 0;
+            });
+        }
+    );
+    $( "#residuos" ).click(
+        function() {
+            console.log("residuos in");
+        }
+    );
+});
