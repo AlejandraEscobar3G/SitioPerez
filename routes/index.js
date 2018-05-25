19beabbe6dab88a4f55528a1791b85f0f3168cce
AlejandraEscobar3G <ale.fesico@gmail.com>
2018-05-25 01:34:40 -0500

    Integro libreria nodemailer

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'TyL Pérez' });
+});
+
+/* Contact form */
+router.post('#s-contacto', function(req, res, next) {
+  
 });
 
 module.exports = router;
