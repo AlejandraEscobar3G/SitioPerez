e0a6ecfab586f316632bbc1adb1e63c955364655
AlejandraEscobar3G <ale.fesico@gmail.com>
2018-06-12 18:49:43 -0500

    Agrego nuevas imágenes en sitio perez

--- public/javascripts/servicios.js
+++ public/javascripts/servicios.js
@@ -1,17 +1,17 @@
-function changeVehiculo() {
-  $("#vehiculo").css({width:"70%"}).attr("src", flotilla[contador].source);
-  contador++;
-  if (contador >= flotilla.length) {
-    contador = 0;
-  }
+function changeVehiculo()  {
+    $("#vehiculo").css({ width: "70%" }).attr("src", flotilla[contador].source);
+    contador++;
+    if (contador >= flotilla.length) {
+        contador = 0;
+    }
 
-  setTimeout("changeVehiculo()", 2000);
+    setTimeout("changeVehiculo()", 2000);
 }
 
 const source = "/images/servicios/";
-let flotilla = [{source:`${source}rabon35.png`, id:1},{source:`${source}rabon5.png`,id:2}, {source:`${source}rabon9.png`,id:3},{source:`${source}trailer.png`,id:4},{source:`${source}cajaseca.png`,id:5},{source:`${source}refri.png`,id:6},{source:`${source}tolva.png`,id:7},{source:`${source}portacontenedor.png`,id:8}];
+let flotilla = [{ source: `${source}rabon35.png`, id: 1 }, { source: `${source}rabon5.png`, id: 2 }, { source: `${source}thorton14.png`, id: 3 }, { source: `${source}trailer.png`, id: 4 }, { source: `${source}cajaseca.png`, id: 5 }, { source: `${source}refri.png`, id: 6 }, { source: `${source}tolva.png`, id: 7 }, { source: `${source}portacontenedor.png`, id: 8 }];
 let contador = 0;
 
 $(function() {
-  changeVehiculo();
+    changeVehiculo();
 });
